187018d75a698db7556449a67d7f42a5ff82e0d2
jerome valette <valettejerome31@gmail.com>
2019-10-12 15:14:26 +0200

    first commit

---
+++ src/main/java/fr/maryCrea/maryCrea/MaryCreaApplication.java
@@ -0,0 +1,13 @@
+package fr.maryCrea.maryCrea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaryCreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaryCreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/maryCrea/maryCrea/MaryCreaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.maryCrea.maryCrea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaryCreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
