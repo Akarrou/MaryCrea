71ac7c969450351495d1e50fa54f130feefe1b75
Jerome Valette <marvindemars@live.fr>
2019-11-23 17:16:07 +0100

    lecture des service de la base de donnée

--- src/main/java/fr/maryCrea/maryCrea/controller/IndexController.java
+++ src/main/java/fr/maryCrea/maryCrea/controller/IndexController.java
@@ -1,9 +1,13 @@
 package fr.maryCrea.maryCrea.controller;
 
+import fr.maryCrea.maryCrea.entity.Service;
 import fr.maryCrea.maryCrea.entity.User;
+import fr.maryCrea.maryCrea.repository.ServicesRepository;
 import fr.maryCrea.maryCrea.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,17 +18,26 @@ public class IndexController {
     @Autowired
     private UserRepository repository;
     private User user;
+    @Autowired
+    private ServicesRepository servicesRepository;
+
+
+    @GetMapping("/")
+    public String runmi(Model out) {
+        out.addAttribute("services", servicesRepository.findAll());
+        return "index";
+    }
 
     @PostMapping("/index")
     public String home(HttpSession session, @RequestParam(name = "email", required = false, defaultValue = "o") String emailValue,
-            @RequestParam(name = "password", required = false, defaultValue = "0") String passwordValue) {
+                       @RequestParam(name = "password", required = false, defaultValue = "0") String passwordValue) {
         user = repository.findByEmail(emailValue);
         String message = "";
         if (user != null) {
             if (user.getPassword().equals(passwordValue)) {
                 session.setAttribute("user", user);
                 return "index";
-            }else {
+            } else {
                 message = "Votre mot de passe est incorect";
             }
         }
